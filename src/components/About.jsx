06919bc4243434ebe73b4463f4dc8955d0089080
Fadil adi maulana <wilzuren@gmail.com>
2026-09-24 18:14:31 +0700

    tambah komponen About

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+    return(
+        <section id="about" className="max-w-3xl mx-auto px-6 py-12 text-center">
+            <h2 className="text-3xl font-bold"> Tentang Saya</h2>"
+            <p className="text-gray-400 mt-4">
+                Saya sedang belajar membangun website dengan React dan Tailwind CSS.
+                Saya suka mencoba hal baru dan mencatat setiap progress belajar saya di Github.
+
+               
+            </p>
+
+        </section>
+    )
+}
+export default About
