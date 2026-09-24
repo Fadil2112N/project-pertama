78f47a2a4e957409b05a5d196ecc12054f66c4d0
Fadil adi maulana <wilzuren@gmail.com>
2026-09-24 17:51:29 +0700

    simpan App.jsx yang memanggil Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import heroImg from './assets/fotohero.png'
+import Hero from "./components/Hero";
 import './App.css'
 
 function App() {
-  return (
-    <section id="center">
-      <div className="hero">
-        <img src={heroImg} className="w-full h-96 object-cover" />
-      </div>
-      <div>
-        <h1>Halo, Nama Saya Fadil Adi Maulana</h1>
-        <p className="text-gray-400 mt-2">
-          Web Developer | Suka membangun hal-hal baru dengan React & Tailwind
-        </p>
-      </div>
-    </section>
-  )
+  return (<Hero />)
 }
+
 
 export default App
